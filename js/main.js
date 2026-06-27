1c246d659a945e64d34e585d5edbd498958e0ac1
Sushincm <sushinofficial@gmail.com>
2026-06-27 17:38:12 +0530

    feat: add 3D translate3d (x, y, z) and rotateX properties to scroll-reveal animation

--- js/main.js
+++ js/main.js
@@ -267,21 +267,30 @@ function initKeyFactsAnimation() {
   mm.add("(min-width: 768px)", () => {
     gsap.set(cards[0], {
       opacity: 0,
+      x: -50,
       y: 150,
+      z: -150,
+      rotateX: 45,
       rotateY: 15,
       skewY: -10,
       transformOrigin: "center center"
     });
     gsap.set(cards[1], {
       opacity: 0,
+      x: 0,
       y: 180,
+      z: -150,
+      rotateX: 45,
       rotateY: 0,
       skewY: 0,
       transformOrigin: "center center"
     });
     gsap.set(cards[2], {
       opacity: 0,
+      x: 50,
       y: 150,
+      z: -150,
+      rotateX: 45,
       rotateY: -15,
       skewY: 10,
       transformOrigin: "center center"
@@ -299,21 +308,30 @@ function initKeyFactsAnimation() {
 
     tl.to(cards[0], {
       opacity: 1,
+      x: 0,
       y: 0,
+      z: 0,
+      rotateX: 0,
       rotateY: 0,
       skewY: 0,
       ease: "power2.out"
     }, 0)
     .to(cards[1], {
       opacity: 1,
+      x: 0,
       y: 0,
+      z: 0,
+      rotateX: 0,
       rotateY: 0,
       skewY: 0,
       ease: "power2.out"
     }, 0.1)
     .to(cards[2], {
       opacity: 1,
+      x: 0,
       y: 0,
+      z: 0,
+      rotateX: 0,
       rotateY: 0,
       skewY: 0,
       ease: "power2.out"
